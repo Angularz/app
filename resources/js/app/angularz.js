b444ba275a7ba3e9a5f91ed28f423c3b377bd753
ricardo3845 <ricardo3845@gmail.com>
2015-11-20 14:15:39 -0500

    Angular Translate structure

--- resources/js/app/angularz.js
+++ resources/js/app/angularz.js
@@ -1,15 +1,52 @@
 (function() {
-	var angularz = angular.module('angularz', []);
+	var translations = {
+	  viewMore: "{{value}}",
+	  viewMoreTrue: 'View More!',
+	  viewMoreFalse: 'Show less',
+	  active: "{{value}}",
+	  activeTrue: 'Activate!',
+	  activeFalse: 'Deactivate!',
+	  noGamesMsg: 'buy a new game!'
+	};
 
-	angularz.controller('angularzController', function() {
-		this.novo = example;
-	});
+	var angularz = angular.module('angularz', ['pascalprecht.translate']);
 
-	var example = {
-		newTest: 'teste',
-    	active: true,
-		name: 'Nome teste 1',
-		id: '12',
-		email: 'ricardo@examplo.com'
-	}
+	angularz.config(['$translateProvider', function ($translateProvider) {
+	  // add translation table
+		$translateProvider.translations('en', translations);
+  		$translateProvider.preferredLanguage('en');
+	}]);
+
+	angularz.controller('angularzController', ['$scope', function ($scope) {
+
+		$scope.example = {
+			name: 'Games:',
+			id: '12',
+			email: 'ricardo@examplo.com',
+			games: {
+				namesLess: [
+					'Halo 5',
+					'Fallout 4',
+					'Mario Maker',
+					'...'
+				],
+				namesMore: [
+					'Halo 5',
+					'Fallout 4',
+					'Mario Maker',
+					'Tomb Raider',
+					'Quantum Break'
+				]
+			},
+			active: true,
+			viewMore: true		
+		};
+
+		$scope.toogle = function() {
+			$scope.example.viewMore ? $scope.example.viewMore = false : $scope.example.viewMore = true;
+		};
+		$scope.toogleActive = function() {
+			$scope.example.active ? $scope.example.active = false : $scope.example.active = true;
+		};
+	}]);
 })();
